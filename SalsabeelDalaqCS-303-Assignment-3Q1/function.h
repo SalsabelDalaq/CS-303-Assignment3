9af03cfe28fc29575041379341eccac079fbd009
SalsabelDalaq <95722891+SalsabelDalaq@users.noreply.github.com>
2023-11-13 19:50:16 -0600

    Update function.h

--- SalsabeelDalaqCS-303-Assignment-3Q1/function.h
+++ SalsabeelDalaqCS-303-Assignment-3Q1/function.h
@@ -2,11 +2,15 @@
 #define FUNCTION_H
 
 #include <string>
-
+// Function to check if a character is an operand (numeric digit)
 bool isOperand(char ch);
+// Function to check if a character is an operator (+, -, *, /, %)
 bool isOperator(char ch);
+// Function to determine the precedence of an operator
 int precedence(char op);
+// Function to convert an infix expression to postfix
 std::string infixToPostfix(std::string exp);
+// Function to check if parentheses in an expression are balanced
 bool isBalanced(std::string exp);
 
 #endif  
