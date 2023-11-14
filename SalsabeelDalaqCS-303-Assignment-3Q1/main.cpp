0a76006b74431deb6f88c029da3351171ebcccef
SalsabelDalaq <95722891+SalsabelDalaq@users.noreply.github.com>
2023-11-13 19:46:36 -0600

    Update main.cpp

--- SalsabeelDalaqCS-303-Assignment-3Q1/main.cpp
+++ SalsabeelDalaqCS-303-Assignment-3Q1/main.cpp
@@ -3,14 +3,18 @@
 #include <stack>
 
 int main() {
+    // Prompt the user to enter an infix expression
     std::string infixExp;
     std::cout << "Enter infix expression: ";
     std::getline(std::cin, infixExp);
-
+    // Check if the parentheses in the infix expression are balanced
     if (!isBalanced(infixExp)) {
         std::cout << "Invalid Expression" << std::endl;
     } else {
+        // Convert the balanced infix expression to postfix
         std::string postfixExp = infixToPostfix(infixExp);
+        
+        // Display the resulting postfix expression
         std::cout << "Postfix expression: " << postfixExp << std::endl;
     }
 
